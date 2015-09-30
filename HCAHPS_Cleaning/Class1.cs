f98c5a1e00ba775c80ed481420ef513d35e234fd
Reza Khoie <reza90@gmail.com>
2015-09-30 15:35:50 -0500

    Handling Missing Values
    This versions finds the nearest record in database and replace missing
    values with corresponding value in the nearest record.

---
+++ HCAHPS_Cleaning/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace HCAHPS_Cleaning
+{
+    public class MySqlDB
+    {
+        private static string settings = "server=127.0.0.1; user id=root; password=`123456; database=hcahps; pooling=false; Convert Zero Datetime=True";
+
+
+        public static void NonQuery(string sql)
+        {
+            using (var myConnection = new MySqlConnection(settings))
+            {
+                MySqlCommand command = new MySqlCommand(sql, myConnection);
+                command.CommandTimeout = 0;
+                myConnection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public static object ScalarQuery(string sql)
+        {
+            using (var myConnection = new MySqlConnection(settings))
+            {
+                MySqlCommand command = new MySqlCommand(sql, myConnection);
+                myConnection.Open();
+                var result = command.ExecuteScalar();
+                return result;
+            }
+        }
+
+        public static DataTable Query(string sql, string table)
+        {
+            using (var myConnection = new MySqlConnection(settings))
+            {
+                myConnection.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, myConnection);
+                DataSet ds = new DataSet();
+                da.Fill(ds, table);
+                DataTable dt = ds.Tables[table];
+                return dt;
+            }
+        }
+    }
+}
---
+++ HCAHPS_Cleaning/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Collections;
+using System.Threading;
+using System.IO;
+
+
+namespace HCAHPS_Cleaning
+{
+    class Program
+    {
+
+        static string ConvertStringArrayToString(string[] array)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (string value in array)
+            {
+                builder.Append(value);
+                builder.Append(',');
+            }
+            return builder.ToString();
+        }
+        public static double calcDistance(string[] a, string[] b)
+        {
+            if (a.Length != b.Length)
+                return -1;
+            else
+            {
+                double dist = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i].All(char.IsDigit))
+                        dist += Math.Pow(int.Parse(a[i]) - int.Parse(b[i]), 2);
+                    else if (a[i] != b[i])
+                        dist += 25;
+                }
+                return Math.Sqrt(dist);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            DataTable data = new DataTable();
+            data = MySqlDB.Query("SELECT * FROM `" + "h2007" + "`", "h2007");
+            int columnNo = data.Columns.Count;
+            int dataCount=data.Rows.Count;
+            string[] inputRow = new string[columnNo];
+            string filePath = "C:/Users/Reza/Desktop/" + "handled.csv";
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+            List<string[]> tempRecords = new List<string[]>();
+            List<string[]> completeRecords = new List<string[]>();
+            for (int i = 0; i < dataCount; i++)
+            {
+                inputRow = data.Rows[i].ItemArray.Select(x => x.ToString()).ToArray();
+                tempRecords.Add(inputRow);
+            }
+
+            for (int i = 0; i < dataCount; i++)
+            {
+                if (!tempRecords[i].Contains("NA"))
+                {
+                    completeRecords.Add(tempRecords[i]);
+                }
+            }
+            int completeDataCount = completeRecords.Count;
+            Parallel.For(0, dataCount, i =>
+                {
+                    if (tempRecords[i].Contains("NA"))
+                    {
+                        double[] distances = new double[completeDataCount];
+                        for (int j = 0; j < completeDataCount; j++)
+                        {
+                            distances[j] = calcDistance(tempRecords[i], completeRecords[j]);
+                        }
+                        string[] tempstr = completeRecords[Array.IndexOf(distances, distances.Min())];
+                        for (int j = 0; j < columnNo; j++)
+                        {
+                            if (tempRecords[i][j].Equals("NA"))
+                            {
+                                tempRecords[i][j] = tempstr[j];
+                            }
+                        }
+                    }
+                    if (i % 1000 == 0)
+                    {
+                        double a = (double)(100 * i) / ((double)dataCount);
+                        Console.WriteLine("%"+a+" cleaned");
+                    }
+                });
+            for (int i = 0; i < dataCount; i++)
+            {
+                File.AppendAllText(filePath, ConvertStringArrayToString(tempRecords[i]));
+            }
+          
+            Console.ReadLine();
+        }
+    }
+}
